63e7c372af9b08fe6ed0121e735f9211d7982a9f
Eugene-Dash <eugene_personal@pm.me>
2024-10-28 20:12:23 -0400

    Removed Cython dependency

--- powerhouse/__init__.py
+++ powerhouse/__init__.py
@@ -1,2 +1,11 @@
-import pyximport; pyximport.install()
-from .powerhouse import np, cp, a2np, a2cp
+# Returns a given array as a powerhouse array
+def aspha(a):
+    if isinstance(a, powerhouse.ndarray):
+        return a
+    return powerhouse.asarray(a)
+
+# Returns a given array as a numpy array
+def asnpa(a):
+    if isinstance(a, np.ndarray):
+        return a
+    return np.asarray(a)
